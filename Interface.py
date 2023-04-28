52709625fd01aa6c36762d19f1f6b3ad00e4631b
Fent1 <43925272+Fent1@users.noreply.github.com>
2023-04-28 19:09:12 -0400

    Add files via upload

--- Interface.py
+++ Interface.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import streamlit as st
 import pickle
-import os
 
 
-os.system("pip install sklearn")
 df = pd.read_csv("cleaned_df_withoutY.csv")
-st.title("Risk of HELOC Prediction")
+st.title("FICO Models/Recommendations")
 with open('lr_model.p', 'rb') as f2:
     loaded_model = pickle.load(f2)
 
+st.subheader('Choose the person :red[features] you want to include:')
 options = st.multiselect(
-    'Choose the features you want to include:',
+    '',
+    list(df.columns[1:24]),
     list(df.columns[1:24])
 )
 
@@ -19,106 +19,106 @@ st.divider()
 predict_dic = dict()
 for i in options:
     if i == "MSinceMostRecentDelq":
-        value = st.radio("Is it a special value? ", ('No', '-7 Condition not Met (e.g. No Inquiries, No Delinquencies)', '-8 No Usable/Valid Accounts Trades or Inquiries'))
+        value = st.radio("$\mathbf Special$  $\mathbf value?$ ", ('No', '-7 Condition not Met (e.g. No Inquiries, No Delinquencies)', '-8 No Usable/Valid Accounts Trades or Inquiries'), key = 1)
         if value == '-7 Condition not Met (e.g. No Inquiries, No Delinquencies)':
             predict_dic['MSinceMostRecentDelq=-7'] = 1
             predict_dic['MSinceMostRecentDelq'] = df.loc[4, i]
-            st.slider(i, int(min(df[i])), int(max(df[i])), disabled=True)
+            st.slider('$\mathbf '+ i + '$', int(min(df[i])), int(max(df[i])), disabled=True)
             st.divider()
         elif value == '-8 No Usable/Valid Accounts Trades or Inquiries':
             predict_dic['MSinceMostRecentDelq=-8'] = 1
             predict_dic['MSinceMostRecentDelq'] = df.loc[4, i]
-            st.slider(i, int(min(df[i])), int(max(df[i])), disabled=True)
+            st.slider('$\mathbf '+ i + '$', int(min(df[i])), int(max(df[i])), disabled=True)
             st.divider()
         else:
-            predict_dic['MSinceMostRecentDelq'] = st.slider(i, int(min(df[i])), int(max(df[i])), disabled=False)
+            predict_dic['MSinceMostRecentDelq'] = st.slider('$\mathbf '+ i + '$', int(min(df[i])), int(max(df[i])), disabled=False)
             st.divider()
     elif i == "MSinceMostRecentInqexcl7days":
-        value = st.radio("Is it a special value? ", ('No', '-7 Condition not Met (e.g. No Inquiries, No Delinquencies)', '-8 No Usable/Valid Accounts Trades or Inquiries'))
+        value = st.radio("$\mathbf Special$  $\mathbf value?$ ", ('No', '-7 Condition not Met (e.g. No Inquiries, No Delinquencies)', '-8 No Usable/Valid Accounts Trades or Inquiries'), key =2)
         if value == '-7 Condition not Met (e.g. No Inquiries, No Delinquencies)':
             predict_dic['MSinceMostRecentInqexcl7days=-7'] = 1
             predict_dic['MSinceMostRecentInqexcl7days'] = df.loc[1, i]
-            st.slider(i, int(min(df[i])), int(max(df[i])), disabled= True)
+            st.slider('$\mathbf '+ i + '$', int(min(df[i])), int(max(df[i])), disabled= True)
             st.divider()
         elif value == '-8 No Usable/Valid Accounts Trades or Inquiries':
             predict_dic['MSinceMostRecentInqexcl7days=-8'] = 1
             predict_dic['MSinceMostRecentInqexcl7days'] = df.loc[1, i]
-            st.slider(i, int(min(df[i])), int(max(df[i])), disabled=True)
+            st.slider('$\mathbf '+ i + '$', int(min(df[i])), int(max(df[i])), disabled=True)
             st.divider()
         else:
-            predict_dic['MSinceMostRecentInqexcl7days'] = (st.slider(i, int(min(df[i])), int(max(df[i])), disabled=False))
+            predict_dic['MSinceMostRecentInqexcl7days'] = (st.slider('$\mathbf '+ i + '$', int(min(df[i])), int(max(df[i])), disabled=False))
             st.divider()
     elif i == "MSinceOldestTradeOpen":
-        value = st.radio("Is it a special value? ", ('No', '-8 No Usable/Valid Accounts Trades or Inquiries'))
+        value = st.radio("$\mathbf Special$  $\mathbf value?$ ", ('No', '-8 No Usable/Valid Accounts Trades or Inquiries'), key=3)
         if value == '-8 No Usable/Valid Accounts Trades or Inquiries':
             predict_dic['MSinceOldestTradeOpen=-8'] = 1
             predict_dic['MSinceOldestTradeOpen'] = df.loc[1, i]
-            st.slider(i, int(min(df[i])), int(max(df[i])), disabled=True)
+            st.slider('$\mathbf '+ i + '$', int(min(df[i])), int(max(df[i])), disabled=True)
             st.divider()
         else:
-            predict_dic['MSinceOldestTradeOpen'] = (st.slider(i, int(min(df[i])), int(max(df[i])), disabled=False))
+            predict_dic['MSinceOldestTradeOpen'] = (st.slider('$\mathbf '+ i + '$', int(min(df[i])), int(max(df[i])), disabled=False))
             st.divider()
     elif i == "NetFractionRevolvingBurden":
-        value = st.radio("Is it a special value? ", ('No', '-8 No Usable/Valid Accounts Trades or Inquiries'))
+        value = st.radio("$\mathbf Special$  $\mathbf value?$ ", ('No', '-8 No Usable/Valid Accounts Trades or Inquiries'), key=4)
         if value == '-8 No Usable/Valid Accounts Trades or Inquiries':
             predict_dic['NetFractionRevolvingBurden=-8'] = 1
             predict_dic['NetFractionRevolvingBurden'] = df.loc[15, i]
-            st.slider(i, int(min(df[i])), int(max(df[i])), disabled=True)
+            st.slider('$\mathbf '+ i + '$', int(min(df[i])), int(max(df[i])), disabled=True)
             st.divider()
         else:
-            predict_dic['NetFractionRevolvingBurden'] = (st.slider(i, int(min(df[i])), int(max(df[i])), disabled=False))
+            predict_dic['NetFractionRevolvingBurden'] = (st.slider('$\mathbf '+ i + '$', int(min(df[i])), int(max(df[i])), disabled=False))
             st.divider()
     elif i == "NetFractionInstallBurden":
-        value = st.radio("Is it a special value? ", ('No', '-8 No Usable/Valid Accounts Trades or Inquiries'))
+        value = st.radio("$\mathbf Special$  $\mathbf value?$ ", ('No', '-8 No Usable/Valid Accounts Trades or Inquiries'), key=5)
         if value == '-8 No Usable/Valid Accounts Trades or Inquiries':
             predict_dic['NetFractionInstallBurden=-8'] = 1
             predict_dic['NetFractionInstallBurden'] = df.loc[3, i]
-            st.slider(i, int(min(df[i])), int(max(df[i])), disabled=True)
+            st.slider('$\mathbf '+ i + '$', int(min(df[i])), int(max(df[i])), disabled=True)
             st.divider()
         else:
-            predict_dic['NetFractionInstallBurden'] = (st.slider(i, int(min(df[i])), int(max(df[i])), disabled=False))
+            predict_dic['NetFractionInstallBurden'] = (st.slider('$\mathbf '+ i + '$', int(min(df[i])), int(max(df[i])), disabled=False))
             st.divider()
     elif i == "NumRevolvingTradesWBalance":
-        value = st.radio("Is it a special value? ", ('No', '-8 No Usable/Valid Accounts Trades or Inquiries'))
+        value = st.radio("$\mathbf Special$  $\mathbf value?$ ", ('No', '-8 No Usable/Valid Accounts Trades or Inquiries'), key=6)
         if value == '-8 No Usable/Valid Accounts Trades or Inquiries':
             predict_dic['NumRevolvingTradesWBalance=-8'] = 1
             predict_dic['NumRevolvingTradesWBalance'] = df.loc[15, i]
-            st.slider(i, int(min(df[i])), int(max(df[i])), disabled=True)
+            st.slider('$\mathbf '+ i + '$', int(min(df[i])), int(max(df[i])), disabled=True)
             st.divider()
         else:
-            predict_dic['NumRevolvingTradesWBalance'] = (st.slider(i, int(min(df[i])), int(max(df[i])), disabled=False))
+            predict_dic['NumRevolvingTradesWBalance'] = (st.slider('$\mathbf '+ i + '$', int(min(df[i])), int(max(df[i])), disabled=False))
             st.divider()
     elif i == "NumInstallTradesWBalance":
-        value = st.radio("Is it a special value? ", ('No', '-8 No Usable/Valid Accounts Trades or Inquiries'))
+        value = st.radio("$\mathbf Special$  $\mathbf value?$ ", ('No', '-8 No Usable/Valid Accounts Trades or Inquiries'), key=7)
         if value == '-8 No Usable/Valid Accounts Trades or Inquiries':
             predict_dic['NumInstallTradesWBalance=-8'] = 1
             predict_dic['NumInstallTradesWBalance'] = df.loc[13,i]
-            st.slider(i, int(min(df[i])), int(max(df[i])), disabled=True)
+            st.slider('$\mathbf '+ i + '$', int(min(df[i])), int(max(df[i])), disabled=True)
             st.divider()
         else:
-            predict_dic['NumInstallTradesWBalance'] = (st.slider(i, int(min(df[i])), int(max(df[i])), disabled=False))
+            predict_dic['NumInstallTradesWBalance'] = (st.slider('$\mathbf '+ i + '$', int(min(df[i])), int(max(df[i])), disabled=False))
             st.divider()
     elif i == "NumBank2NatlTradesWHighUtilization":
-        value = st.radio("Is it a special value? ", ('No', '-8 No Usable/Valid Accounts Trades or Inquiries'))
+        value = st.radio("$\mathbf Special$  $\mathbf value?$ ", ('No', '-8 No Usable/Valid Accounts Trades or Inquiries'), key=8)
         if value == '-8 No Usable/Valid Accounts Trades or Inquiries':
             predict_dic['NumBank2NatlTradesWHighUtilization=-8'] = 1
-            st.slider(i, int(min(df[i])), int(max(df[i])), disabled=True)
+            st.slider('$\mathbf '+ i + '$', int(min(df[i])), int(max(df[i])), disabled=True)
             predict_dic['NumBank2NatlTradesWHighUtilization'] = df.loc[15, i]
             st.divider()
         else:
-            predict_dic['NumBank2NatlTradesWHighUtilization'] = (st.slider(i, int(min(df[i])), int(max(df[i])), disabled=False))
+            predict_dic['NumBank2NatlTradesWHighUtilization'] = (st.slider('$\mathbf '+ i + '$', int(min(df[i])), int(max(df[i])), disabled=False))
             st.divider()
     elif i == "PercentTradesWBalance":
-        value = st.radio("Is it a special value? ", ('No', '-8 No Usable/Valid Accounts Trades or Inquiries'))
+        value = st.radio("$\mathbf Special$  $\mathbf value?$ ", ('No', '-8 No Usable/Valid Accounts Trades or Inquiries') ,key=9)
         if value == '-8 No Usable/Valid Accounts Trades or Inquiries':
             predict_dic['PercentTradesWBalance=-8'] = 1
-            predict_dic['PercentTradesWBalance'] = (st.slider(i, int(min(df[i])), int(max(df[i])), disabled=True))
+            predict_dic['PercentTradesWBalance'] = (st.slider('$\mathbf '+ i + '$', int(min(df[i])), int(max(df[i])), disabled=True))
             st.divider()
         else:
-            predict_dic['PercentTradesWBalance'] = (st.slider(i, int(min(df[i])), int(max(df[i])), disabled=False))
+            predict_dic['PercentTradesWBalance'] = (st.slider('$\mathbf '+ i + '$', int(min(df[i])), int(max(df[i])), disabled=False))
             st.divider()
     else:
-        predict_dic[i] = (st.slider(i, int(min(df[i])), int(max(df[i]))))
+        predict_dic[i] = (st.slider('$\mathbf '+ i + '$', int(min(df[i])), int(max(df[i]))))
         st.divider()
 
 # Get a list to predict Risk
@@ -130,8 +130,22 @@ for i in df.columns[1:]:
         risk_list.append(predict_dic[i])
 
 prediction = loaded_model.predict([risk_list])[0]
+with st.sidebar:
+    st.title("Prediction Result")
+    if prediction == 1:
+        st.markdown('This person is :red[REJECTED]:thumbsdown:, since he/she is classified as a :red[BAD] risk performance. ')
+        st.markdown('The features are shown below:')
+        st.divider()
+        for k, v in predict_dic.items():
+            st.text(k + ' is ' + str(v))
+        st.divider()
+        st.markdown('$Note:$ $Features$  $NOT$  $selected$  $are$ $set$ $to$ $\mathbf 0$ $by$ $default$')
+    else:
+        st.markdown('This person is :green[accepted]:thumbsup:, since he/she is classified as a :green[GOOD] risk performance. ')
+        st.markdown('The features are shown below:')
+        st.divider()
+        for k, v in predict_dic.items():
+            st.text(k + ' is ' + str(v))
+        st.divider()
+        st.markdown('$Note:$ $Features$  $NOT$  $selected$  $are$ $set$ $to$ $\mathbf 0$ $by$ $default$')
 
-if prediction == 1:
-    st.text('This person is rejected')
-else:
-    st.text('This person is accepted')
